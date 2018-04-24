f3f3f1325be04090e3828ef83f5403e1fcb1bfdb
Ivailo Kodov <ikodov@melontech.com>
2018-04-24 18:12:22 +0300

    Some refactorings.

--- BookShop.Api/Startup.cs
+++ BookShop.Api/Startup.cs
@@ -21,7 +21,8 @@ namespace BookShop.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<BookShopDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<BookShopDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddAutoMapper();
 
