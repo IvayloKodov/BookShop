73f156e2769504ac56fc0fb6fb5e6b4cfd1e9449
Ivailo Kodov <ikodov@melontech.com>
2018-04-27 15:20:17 +0300

    Some refactorings.

---
+++ BookShop.Api/Infrastructure/Filters/LogActionFilter.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BookShop.Api.Infrastructure.Filters
+{
+    public class LogActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Log(nameof(OnActionExecuting), (Controller)filterContext.Controller);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Log(nameof(OnActionExecuted), (Controller)filterContext.Controller);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            Log(nameof(OnResultExecuting), (Controller)filterContext.Controller);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Log(nameof(OnResultExecuted), (Controller)filterContext.Controller);
+        }
+
+        private static void Log(string methodName, Controller controller)
+        {
+            var controllerName = controller.ControllerContext.ActionDescriptor.ControllerName;
+
+            var actionName = controller.ControllerContext.ActionDescriptor.ActionName;
+
+            var message = $"{methodName} controller:{controllerName} action:{actionName}";
+
+            Debug.WriteLine(message, "Action Filter Log");
+        }
+    }
+}
