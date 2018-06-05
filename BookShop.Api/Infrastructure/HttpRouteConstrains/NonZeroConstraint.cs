ba4c0124c0034ba9118aada0040195d646548d7f
Ivailo Kodov <ikodov@melontech.com>
2018-06-05 16:39:17 +0300

    Adding some release date attributes.

---
+++ BookShop.Api/Infrastructure/HttpRouteConstrains/NonZeroConstraint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace BookShop.Api.Infrastructure.HttpRouteConstrains
+{
+    public class NonZeroConstraint : IRouteConstraint
+    {
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values,
+            RouteDirection routeDirection)
+        {
+            object value;
+            if (values.TryGetValue(routeKey, out value) && value != null)
+            {
+                long longValue;
+                if (value is long)
+                {
+                    longValue = (long)value;
+                    return longValue != 0;
+                }
+
+                string valueString = Convert.ToString(value, CultureInfo.InvariantCulture);
+                if (Int64.TryParse(valueString, NumberStyles.Integer,
+                    CultureInfo.InvariantCulture, out longValue))
+                {
+                    return longValue != 0;
+                }
+            }
+            return false;
+        }
+    }
+}
